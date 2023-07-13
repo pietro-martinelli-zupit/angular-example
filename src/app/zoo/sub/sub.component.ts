2a262b539f52f1b816d836691fec8b9756205737
Pietro Martinelli <pietro.martinelli@zupit.it>
2023-07-13 13:42:18 +0200

    More about NgRx store

--- src/app/zoo/sub/sub.component.ts
+++ src/app/zoo/sub/sub.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {State} from "../../users/users.reducer";
+import {select} from "../../users/users.actions";
 
 @Component({
   selector: 'app-sub',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./sub.component.css']
 })
 export class SubComponent {
-
+  constructor(private store: Store<{ users: State }>) {
+  }
+  onClick() {
+    this.store.dispatch(select({
+      user: {
+        id: 19,
+        name: 'Pietro',
+        username: 'pietrom',
+        email: 'pietro@example.com',
+        phone: '00391234567',
+        website: 'askpietro.amicofragile.org',
+        address: {
+          street: 'via Pietro 19',
+          city: 'Brescia',
+          suite: 'A',
+          zipcode: '12345',
+          geo: {
+            lat: 13.45,
+            lng: 44.44
+          }
+        }
+      }
+    }))
+  }
 }
